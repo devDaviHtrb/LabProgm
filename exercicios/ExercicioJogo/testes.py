1feec30d085b156d7456df29d98142ce4c8c1fa6
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-05-16 07:28:23 -0300

    Update testes.py

--- exercicios/ExercicioJogo/testes.py
+++ exercicios/ExercicioJogo/testes.py
@@ -41,6 +41,74 @@ print("")
 #Gabriel tenta comprar Minecraft 2 novamente
 AgostinhoGames.realizarCompra("Minecraft 2", 80027092)
 print("")
+from jogo import *
+from jogador import *
+from plataformaDejogos import *
+
+AgostinhoGames = Plataforma("AgostinhoGames")
+
+#Criando Jogos
+Minecraft2 = Jogo("Minecraft 2", "Sandbox", "+10", 249.99)
+AgostinhoGames.adicionarJogoCatalogo(Minecraft2)
+
+GTA66 = Jogo("GTA 66", "FPS", "+18", 750.99)
+AgostinhoGames.adicionarJogoCatalogo(GTA66)
+
+SlimeRancher3 = Jogo("Slime Rancher 3", "Aventura", "Livre", 130.99)
+AgostinhoGames.adicionarJogoCatalogo(SlimeRancher3)
+
+#Criando Jogadores
+Davi = Jogador("DevDaviHTRB", 90025091)
+AgostinhoGames.adicionarJogador(Davi)
+
+Gabriel = Jogador("GabrielNsCorreia", 80027092)
+AgostinhoGames.adicionarJogador(Gabriel)
+
+#Adicionando Saldo
+pap = 400
+clt = 1300from jogo import *
+from jogador import *
+from plataformaDejogos import *
+
+AgostinhoGames = Plataforma("AgostinhoGames")
+
+#Criando Jogos
+Minecraft2 = Jogo("Minecraft 2", "Sandbox", "+10", 249.99)
+AgostinhoGames.adicionarJogoCatalogo(Minecraft2)
+
+GTA66 = Jogo("GTA 66", "FPS", "+18", 750.99)
+AgostinhoGames.adicionarJogoCatalogo(GTA66)
+
+SlimeRancher3 = Jogo("Slime Rancher 3", "Aventura", "Livre", 130.99)
+AgostinhoGames.adicionarJogoCatalogo(SlimeRancher3)
+
+#Criando Jogadores
+Davi = Jogador("DevDaviHTRB", 90025091)
+AgostinhoGames.adicionarJogador(Davi)
+
+Gabriel = Jogador("GabrielNsCorreia", 80027092)
+AgostinhoGames.adicionarJogador(Gabriel)
+
+#Adicionando Saldo
+pap = 400
+clt = 1300
+Davi.AdicionaSaldo(pap)
+Gabriel.AdicionaSaldo(clt)
+print("")
+
+#Listar Calogo
+AgostinhoGames.listarCatalogo()
+print("")
+
+#Gabriel tenta comprar Minecraft 2
+AgostinhoGames.realizarCompra("Minecraft 2", 80027092)
+print("")
+# Gabriel verifica seu saldo
+Gabriel.exibirPerfil()
+print("")
+#Gabriel tenta comprar Minecraft 2 novamente
+AgostinhoGames.realizarCompra("Minecraft 2", 80027092)
+print("")
 
 #Davi tenta comprar GTA 66
 AgostinhoGames.realizarCompra("GTA 66", 90025091)
@@ -63,3 +131,72 @@ Davi.exibirPerfil()
 print("")
 #Verificando o saldo de Gabriel
 Gabriel.exibirPerfil()
+Gabriel.AvaliarJogo(Minecraft2, 10)
+Davi.AvaliarJogo(Minecraft2, 5)
+Minecraft2.exibirDetalhes()
+Davi.AdicionaSaldo(pap)
+Gabriel.AdicionaSaldo(clt)
+print("")
+
+#Listar Calogo
+AgostinhoGames.listarCatalogo()
+print("")
+
+#Gabriel tenta comprar Minecraft 2
+AgostinhoGames.realizarCompra("Minecraft 2", 80027092)
+print("")
+# Gabriel verifica seu saldo
+Gabriel.exibirPerfil()
+print("")
+#Gabriel tenta comprar Minecraft 2 novamente
+AgostinhoGames.realizarCompra("Minecraft 2", 80027092)
+print("")
+
+#Davi tenta comprar GTA 66
+AgostinhoGames.realizarCompra("GTA 66", 90025091)
+print("")
+#Davi Adiciona saldo
+Davi.AdicionaSaldo(1000)
+print("")
+#Davi Tenta novamente
+AgostinhoGames.realizarCompra("GTA 66", 90025091)
+print("")
+#Davi verifica seu saldo
+Davi.exibirPerfil()
+print("")
+
+#Gabriel compra Slime Rancher 3 de presente para Davi
+AgostinhoGames.realizarCompra("Slime Rancher 3", 80027092, 90025091)
+print("")
+#Verificando se Davi recebeu o jogo
+Davi.exibirPerfil()
+print("")
+#Verificando o saldo de Gabriel
+Gabriel.exibirPerfil()
+Gabriel.AvaliarJogo(Minecraft2, 10)
+Davi.AvaliarJogo(Minecraft2, 5)
+Minecraft2.exibirDetalhes()
+#Davi tenta comprar GTA 66
+AgostinhoGames.realizarCompra("GTA 66", 90025091)
+print("")
+#Davi Adiciona saldo
+Davi.AdicionaSaldo(1000)
+print("")
+#Davi Tenta novamente
+AgostinhoGames.realizarCompra("GTA 66", 90025091)
+print("")
+#Davi verifica seu saldo
+Davi.exibirPerfil()
+print("")
+
+#Gabriel compra Slime Rancher 3 de presente para Davi
+AgostinhoGames.realizarCompra("Slime Rancher 3", 80027092, 90025091)
+print("")
+#Verificando se Davi recebeu o jogo
+Davi.exibirPerfil()
+print("")
+#Verificando o saldo de Gabriel
+Gabriel.exibirPerfil()
+Gabriel.AvaliarJogo(Minecraft2, 10)
+Davi.AvaliarJogo(Minecraft2, 5)
+Minecraft2.exibirDetalhes()
