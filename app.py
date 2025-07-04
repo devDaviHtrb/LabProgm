c345e839bd3767181487692e274f46d41a769c2a
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-07-04 11:35:04 -0300

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route("/user/<username>")
-def profile(username):
-    return render_template("profile.html", user=username)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
